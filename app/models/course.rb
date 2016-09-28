8ec3b5be4e474bed4a16b254d039c181397f456a
App Academy Student <student@appacademy.io>
2016-09-28 11:29:30 -0700

    Added associations for enrollment, user, and course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,16 @@
 class Course < ActiveRecord::Base
+  has_many(:enrollments,
+    class_name: :Enrollment,
+    foreign_key: :course_id,
+    primary_key: :id
+  )
+
+  has_many :enrolled_students, through: :enrollments, source: :user
+
+  belongs_to (
+  :prerequisite,
+  class_name: :Course,
+  foreign_key: :prereq_id,
+  primary_key: :id
+  )
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,13 @@
 class Enrollment < ActiveRecord::Base
+  belongs_to(:course,
+    class_name: :Course,
+    foreign_key: :course_id,
+    primary_key: :id
+  )
+
+  belongs_to(:user,
+    class_name: :User,
+    foreign_key: :user_id,
+    primary_key: :id
+  )
 end
