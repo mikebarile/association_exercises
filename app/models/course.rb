7cdac97eec2ffee4f9aadf6e2917103dfe8fd883
App Academy Student <student@appacademy.io>
2016-09-28 11:42:40 -0700

    Finished exercises

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,12 +5,17 @@ class Course < ActiveRecord::Base
     primary_key: :id
   )
 
-  has_many :enrolled_students, through: :enrollments, source: :user
-
-  belongs_to (
-  :prerequisite,
+  belongs_to(:prerequisite,
   class_name: :Course,
   foreign_key: :prereq_id,
   primary_key: :id
   )
+
+  belongs_to(:instructor,
+  class_name: :User,
+  foreign_key: :instructor_id,
+  primary_key: :id)
+
+  has_many :enrolled_students, through: :enrollments, source: :user
+
 end
